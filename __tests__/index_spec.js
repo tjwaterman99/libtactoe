91cf4f1ec9cb22e009c116bf540ddcaf0b7a91cb
Tom Waterman <tjwaterman99@gmail.com>
2024-09-30 08:48:04 +0000

    Almost there

--- __tests__/index_spec.js
+++ __tests__/index_spec.js
@@ -3,11 +3,23 @@ let { default: Game } = require('../dist/index');
 
 test('Game', () => {
     let players = [{name: 'player1', 'token': 'X'}, {'name': 'player2', 'token': 'O'}]
-    let coordinates = Array(9)
+    let coordinates = new Array()
+    for (var x of Array(9).keys()) {
+        coordinates.push({id: x})
+    }
     let game = new Game({
         players: players,
         coordinates: coordinates
     })
     expect(game.players).toBe(players)
     expect(game.coordinates).toBe(coordinates)
+    expect(game.current_player).toBe(0)
+
+    game.play(0)
+    expect(game.coordinates[0].played = players[0])
+    expect(game.current_player).toBe(1)
+
+    game.play(1)
+    expect(game.coordinates[1].played = players[1])
+    expect(game.current_player).toBe(0)
 });
--- src/index.ts
+++ src/index.ts
@@ -26,5 +26,30 @@ export default class Game {
         this.coordinates = state.coordinates
         this.current_player = 0
     }
+
+    changePlayer() {
+        this.current_player = Math.abs(this.current_player - 1)
+    }
+
+    play(coordinate: number) {
+        let player = this.players[this.current_player]
+        this.coordinates[coordinate].played = player
+        this.changePlayer()
+    }
+
+    // TODO: check the winning conditions to see if any of them
+    // are all occupied by a single player
+    isFinished() {
+        let winning_conditions = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [1,4,8],
+            [6,4,2],
+        ]
+    }
 }
 
